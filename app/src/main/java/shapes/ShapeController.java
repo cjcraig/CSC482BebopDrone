4edb7c706d5777ceca5c3f8b06c3eca9b5fce66d
georgenasr <georgenasr@msn.com>
2015-10-29 13:33:47 -0700

    Added structure to the ShapeController

--- app/src/main/java/shapes/ShapeController.java
+++ app/src/main/java/shapes/ShapeController.java
@@ -5,4 +5,41 @@ package shapes;
  */
 public class ShapeController {
     //I totally LOVE Android Studio!
+    public enum ORIENTATION{Directional, FrontFacing}
+    //Directional means it orients based on the direction it is going. Front facing always faces one direction
+
+    private ShapeController(){}//not to be instantiated
+
+    public static void squareCommands(ORIENTATION o, int actionDuration, byte speed){
+        switch(o){
+            case Directional:
+
+                break;
+            case FrontFacing:
+
+                break;
+        }
+    }
+
+    public static void triangleCommands(ORIENTATION o, int actionDuration, byte speed){
+        switch(o){
+            case Directional:
+
+                break;
+            case FrontFacing:
+
+                break;
+        }
+    }
+
+    public static void circleCommands(ORIENTATION o, int actionDuration, byte speed){
+        switch(o){
+            case Directional:
+
+                break;
+            case FrontFacing:
+
+                break;
+        }
+    }
 }
